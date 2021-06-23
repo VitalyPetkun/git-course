ae0bdf8fcdd719307819b648e65a762df8b990f7
VitalyPetkun <p.u.d.i.n.g.69.v@gmail.com>
2021-06-23 18:08:39 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println("hello" + s.getName());
+        System.out.println("Age " + s.getAge());
+    }
+}
---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String age;
+    private String name;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student(){
+        age = "20";
+        name = "Vitaly";
+    }
+}
